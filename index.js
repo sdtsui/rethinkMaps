07eca918e13fdea8550469d75e581741fb483482
sdtsui <danielt213@gmail.com>
2015-07-06 12:39:43 -0700

    node server up

---
+++ index.js
@@ -0,0 +1,20 @@
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var express      = require('express');
+var app          = express();
+var port         = process.env.PORT || 9000;
+
+// var getTweetsRouter = require('./routes/getTweets');
+// 
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(__dirname));
+
+// app.use('/locations', getTweetsRouter);
+
+
+app.listen(port);
+console.log('Listening on PORT ' + port + '....');
+
