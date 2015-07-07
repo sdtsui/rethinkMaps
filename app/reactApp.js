7b2a6de2646c2b8a8948e63d7df7258927608c03
sdtsui <danielt213@gmail.com>
2015-07-06 19:08:54 -0700

    Complete: Responsive Searching; Next: geocoder

--- app/reactApp.js
+++ app/reactApp.js
@@ -1,50 +1,48 @@
 /** @jsx React.DOM */
 
-var ProductCategoryRow = React.createClass({
-    render: function() {
-        return (<tr><th colSpan="2">{this.props.category}</th></tr>);
-    }
-});
 
-var ProductRow = React.createClass({
+/**
+ * Global Handler Functions
+ */
+
+// var printAllProps = function() {
+
+// }
+
+
+/**
+ * Components
+ */
+
+var LocationRow = React.createClass({
+    handleClick: function() {
+      console.log(this.props);
+    },
     render: function() {
-        var name = this.props.product.stocked ?
-            this.props.product.name :
-            <span style={{color: 'red'}}>
-                {this.props.product.name}
-            </span>;
+        var placeName = this.props.product.placeName;
         return (
             <tr>
-                <td>{name}</td>
-                <td>{this.props.product.price}</td>
+                <td onClick={this.handleClick}>{placeName}</td>
             </tr>
         );
     }
 });
 
-var ProductTable = React.createClass({
+var LocationsTable = React.createClass({
     render: function() {
         console.log(this.props);
         var rows = [];
         var lastCategory = null;
-        this.props.products.forEach(function(product) {
-            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+        this.props.locations.forEach(function(product) {
+            if (product.placeName.indexOf(this.props.filterText) === -1) {
                 return;
             }
-            if (product.category !== lastCategory) {
-                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-            }
-            rows.push(<ProductRow product={product} key={product.name} />);
-            lastCategory = product.category;
+            rows.push(<LocationRow product={product} key={product.placeName} />);
         }.bind(this));
+
+        //Warn: component could use a bunch of styling
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
                 <tbody>{rows}</tbody>
             </table>
         );
@@ -52,79 +50,78 @@ var ProductTable = React.createClass({
 });
 
 var SearchBar = React.createClass({
-    handleChange: function() {
-        this.props.onUserInput(
-            this.refs.filterTextInput.getDOMNode().value,
-            this.refs.inStockOnlyInput.getDOMNode().checked
-        );
-    },
-    render: function() {
-        return (
-            <form>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={this.props.filterText}
-                    ref="filterTextInput"
-                    onChange={this.handleChange}
-                />
-                <p>
-                    <input
-                        type="checkbox"
-                        checked={this.props.inStockOnly}
-                        ref="inStockOnlyInput"
-                        onChange={this.handleChange}
-                    />
-                    {' '}
-                    Only show products in stock
-                </p>
-            </form>
-        );
-    }
+  handleChange: function() {
+    this.props.onUserInput(
+      this.refs.filterTextInput.getDOMNode().value
+    );
+  },
+  render: function() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search for places!"
+          value={this.props.filterText}
+          ref="filterTextInput"
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
 });
 
-var FilterableProductTable = React.createClass({
-    getInitialState: function() {
-        return {
-            filterText: '',
-            inStockOnly: false
-        };
-    },
+var FilterableLocationsTable = React.createClass({
+  getInitialState: function() {
+    return {
+      filterText: ''
+    };
+  },
 
-    handleUserInput: function(filterText, inStockOnly) {
-        this.setState({
-            filterText: filterText,
-            inStockOnly: inStockOnly
-        });
-    },
+  handleUserInput: function(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  },
 
-    render: function() {
-        return (
-            <div>
-                <SearchBar
-                    filterText={this.state.filterText}
-                    inStockOnly={this.state.inStockOnly}
-                    onUserInput={this.handleUserInput}
-                />
-                <ProductTable
-                    products={this.props.products}
-                    filterText={this.state.filterText}
-                    inStockOnly={this.state.inStockOnly}
-                />
-            </div>
-        );
-    }
+  render: function() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onUserInput={this.handleUserInput}
+        />
+        <LocationsTable
+          locations={this.props.locations}
+          filterText={this.state.filterText}
+        />
+      </div>
+    );
+  }
 });
 
 
-var PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+var locations = [
+  {
+    placeName: "2001 Hillside Blvd, Colma, CA 94014",
+    location : [37.680573, -122.446698]
+  },
+  {
+    placeName: "300 Finley Road, San Francisco, CA 94129",
+    location : [37.788773, -122.460603]
+  },
+  {
+    placeName: "99 Harding Rd, San Francisco, CA 94132",
+    location : [37.724035, -122.492532]
+  },
+  {
+    placeName: "100 John F Shelley Dr, San Francisco, CA 94134",
+    location : [37.719146, -122.421121]
+  },
+  {
+    placeName: "2300 Junipero Serra Blvd, Daly City, CA 94015",
+    location : [37.697689, -122.477425]
+  }
 ];
 
-React.render(<FilterableProductTable products={PRODUCTS} />, 
-    document.getElementById('searchContainer'));
+React.render(<FilterableLocationsTable locations={locations} />, 
+  document.getElementById('searchContainer'));
